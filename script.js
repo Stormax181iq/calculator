99bcd3eef802f27e37d18836c58a857cca7a1592
Stormax181iq <kelianchalier@gmail.com>
2024-01-07 12:46:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const calculator = document.getElementById("calculator-wrapper");
+const displayer = document.getElementById("displayer");
+
+calculator.addEventListener('click', function (e) {
+  if (e.target.classList.contains('calc-btn')) {
+    if (e.target.innerText === "=") {
+      calc();
+    } else {
+      displayer.innerText += e.target.innerText;
+    }
+  } else if (e.target.classList.contains('utility-btn')) {
+    if (e.target.innerText === "CE") {
+      displayer.innerText = "";
+    } else if (e.target.innerText === "DEL") {
+      displayer.innerText = displayer.innerText.slice(0, -1);
+    }
+  }
+});
+
+function calc() {
+  let expression = displayer.innerText;
+  expression = expression.replaceAll("×", "*");
+  expression = expression.replaceAll("÷", "/");
+  console.log(expression);
+
+  try {
+    let result = eval(expression);
+    displayer.innerText = result;
+  } catch {
+    displayer.innerText = "Error";
+  }
+}
